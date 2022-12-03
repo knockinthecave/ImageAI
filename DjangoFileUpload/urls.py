68d5128be44076083c6290deebbade91b1156896
knockinthecave <98227391+knockinthecave@users.noreply.github.com>
2022-12-03 23:32:08 +0900

    Add files via upload

---
+++ DjangoFileUpload/urls.py
@@ -0,0 +1,25 @@
+"""DjangoFileUpload URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include("Core.urls")),
+    path('api/cut_image/', include("AI_Module.urls")), 
+]
+
+#일단은 AI_Module.urls를 보면 api/cut_image만 치면 AI모듈이 실행됨.(220525)
